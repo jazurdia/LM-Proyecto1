c7fee6e3929d303abb187f20351f20dbad094c43
Mark Albrand <mark.albrand5@gmail.com>
2023-08-02 13:00:48 -0600

    setup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import ply.lex as lex
 import ply.yacc as yacc
+import networkx as nx
+import matplotlib.pyplot as plt
 
 # Definir el alfabeto de L
 # Tokens (palabras clave)
@@ -29,30 +31,40 @@ t_CONSTANT = r'[01]'
 # Ignorar espacios en blanco
 t_ignore = ' \t'
 
+
 # Función para manejar errores léxicos
 def t_error(t):
     print("Caracter ilegal: '%s'" % t.value[0])
     t.lexer.skip(1)
 
+
 # Crear el lexer
 lexer = lex.lex()
+
+# Crear el grafo
+G = nx.DiGraph()
+
 
 # Definir las reglas gramaticales de L
 def p_formula_variable(p):
     'formula : VARIABLE'
     p[0] = p[1]
 
+
 def p_formula_negation(p):
     'formula : NEGATION formula'
     p[0] = '~' + p[2]
+
 
 def p_formula_and(p):
     'formula : formula AND formula'
     p[0] = p[1] + '^' + p[3]
 
+
 def p_formula_or(p):
     'formula : formula OR formula'
     p[0] = p[1] + 'o' + p[3]
+
 
 def p_formula_implies(p):
     'formula : formula IMPLIES formula'
@@ -61,17 +73,21 @@ def p_formula_implies(p):
     print(p[2])
     print(p[3])
 
+
 def p_formula_equivalent(p):
     'formula : formula EQUIVALENT formula'
     p[0] = p[1] + '<=>' + p[3]
+
 
 def p_formula_parentheses(p):
     'formula : LPAREN formula RPAREN'
     p[0] = '(' + p[2] + ')'
 
+
 def p_formula_constant(p):
     'formula : CONSTANT'
     p[0] = p[1]
+
 
 # Manejo de errores sintácticos
 def p_error(p):
@@ -80,16 +96,20 @@ def p_error(p):
     else:
         print(f"Error de sintaxis en la posición {p.lexpos}: Caracter '{p.value[0]}' no esperado.")
 
+
 # Crear el parser
 parser = yacc.yacc()
+
 
 # Función para imprimir el mensaje en verde
 def print_green(text):
     print("\033[92m" + text + "\033[0m")
 
+
 # Función para imprimir el mensaje en rojo
 def print_red(text):
     print("\033[91m" + text + "\033[0m")
+
 
 if __name__ == '__main__':
     while True:
@@ -109,6 +129,10 @@ if __name__ == '__main__':
             # Imprimir el resultado en verde si es bien formada, en rojo si es mal formada
             if is_well_formed:
                 print_green("Expresión bien formada: " + result)
+
+                # Mostrar el grafo
+                nx.draw_networkx(G, with_labels=True, arrows=True)
+                plt.show()
             else:
                 print_red("Expresión mal formada.")
 
